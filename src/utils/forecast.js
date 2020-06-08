97c79bfa3366347f6680c664a03f6557949d796f
Matthew Kevork <mkevork@ineat.ca>
2020-06-08 13:57:07 -0400

    Add humidity to forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out, and it feels like " + body.current.feelslike + " degrees out."
+            callback(undefined, body.current.weather_descriptions[0] + 
+                ". It is currently " + body.current.temperature + " degrees out, and it feels like " + 
+                body.current.feelslike + " degrees out. The humidity is currently " +
+                body.current.humidity + " percent." 
             // {
             //     description: body.current.weather_descriptions[0],
             //     temperature: body.current.temperature,
