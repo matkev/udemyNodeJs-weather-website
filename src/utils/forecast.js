94d993a622ec6256d0ddf12ca1bf3436d58669a4
Matthew Kevork <mkevork@ineat.ca>
2020-06-08 14:23:17 -0400

    forecast comment fix

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,14 +12,7 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined, body.current.weather_descriptions[0] + 
                 ". It is currently " + body.current.temperature + " degrees out, and it feels like " + 
                 body.current.feelslike + " degrees out. The humidity is currently " +
-                body.current.humidity + " percent." 
-            // {
-            //     description: body.current.weather_descriptions[0],
-            //     temperature: body.current.temperature,
-            //     feelslike: body.current.feelslike
-
-            // }
-            
+                body.current.humidity + " percent."             
             )
         }
     })
