9a269c75d9c6db0d54852b7e2c00045f253e0304
Matthew Kevork <mkevork@ineat.ca>
2020-08-29 14:41:42 -0400

    updated

--- src/app.js
+++ src/app.js
@@ -59,7 +59,7 @@ app.get('/weather', (req, res) => {
             return res.send({ error })
         }
 
-        forecast(latitude, longitude, (error, forecastData) => {
+        (forecast)(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return res.send({ error })
             }
